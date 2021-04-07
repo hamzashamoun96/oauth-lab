1fa93f9be4c8155f74bf0e86c37287db6b8422bc
hamza shamoun <76407512+hamzashamoun96@users.noreply.github.com>
2021-04-07 20:24:39 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-const URL = `https://www.facebook.com/v10.0/dialog/oauth?client_id=499316858094104&redirect_uri=https://fb-oauth-abbmr.herokuapp.com/fb_oauth&state="{st=state123abc,ds=123456789}"`;
+const URL = `https://www.facebook.com/v10.0/dialog/oauth?`
 
 
 
